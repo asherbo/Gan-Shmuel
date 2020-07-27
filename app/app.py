fce2907ecc058c1a26a76856af9989389467f0d8
Administrator <nivgabay1993@gmail.com>
2020-07-27 20:43:53 +0300

    automatic commit pull

--- app/app.py
+++ app/app.py
@@ -1,23 +1,34 @@
 from flask import Flask ,jsonify
 from flask.ext.mysql import MySQL
 from healthcheck import HealthCheck, EnvironmentDump
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 # from sqlalchemy import MetaData
 
+# For the docker file flask app: (DO NOT DELETE!)
+# pip install Flask , or, pip install -U Flask (version 1.1.2)
+# pip install Flask-MySQL  (version 1.5.1)
+# pip install healthcheck (version 1.3.3)
+# pip install Flask-SQLAlchemy (version 2.4.4)
+# pip install SQLAlchemy (version 1.3.18)
+
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+health = HealthCheck(app, "/healthcheck")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://mylogin:mypw@localhost/mydb'
 db = SQLAlchemy(app)
 
 @app.route("/")
 def main():
-    return "<h1>Test<h1>"
+    return "<h1>This is Flask frontend webapp Test!<h1>"
 
 
-@app.route("/niv")
-def nuv():
-    return "<h1>.....<h1>"
+# Niv def
 
 
+
+
+
+# Mor def
 @app.route("/health")
 def is_alive():
     resp = jsonify(success=True)

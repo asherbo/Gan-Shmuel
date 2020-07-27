e81d116f288fa9c814a0c4e95113e31209125350
mor bargig <morbargig@gmail.com>
2020-07-27 14:42:21 +0300

    automatic commit pull

--- Weight/commend.sh
+++ Weight/commend.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+branchName="weight"
+git branch $branchName
 git add .
 commit="automatic commit"
 if [ $1 ]; then commit=$1; fi
 git commit -m "$commit pull"
-git pull origin weight
+git pull origin $branchName
 git add .
 git commit -m "$commit push"
-git push origin weight
+git push origin $branchName
--- commend.sh
+++ commend.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+branchName="master"
+git branch $branchName
 git add .
 commit="automatic commit"
 if [ $1 ]; then commit=$1; fi
 git commit -m "$commit pull"
-git pull origin master
+git pull origin $branchName
 git add .
 git commit -m "$commit push"
-git push origin master
+git push origin $branchName

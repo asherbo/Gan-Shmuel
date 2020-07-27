ceb79aae635cac97a79f4e68b30a0bc29b8e6dbb
mor bargig <morbargig@gmail.com>
2020-07-27 13:43:53 +0300

    commit name

---
+++ Weight/commend.sh
@@ -0,0 +1,4 @@
+git pull origin weigth
+git add .
+git commit -m "commit name"
+git push origin weigth
---
+++ commend.sh
@@ -0,0 +1,4 @@
+git pull origin master
+git add .
+git commit -m "commit name"
+git push origin master

8bd97fbe9ce7f9371682690ac3bf664d140aa1b0
mor bargig <morbargig@gmail.com>
2020-07-27 20:45:38 +0300

    automatic commit pull

--- app/app.py
+++ app/app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-# from flask_restful import Resource, Api
+from flask import Flask ,jsonify
 
 app = Flask(__name__)
 @app.route("/")
@@ -7,13 +6,15 @@ def main():
     return "<h1>Test<h1>"
 
 
-
-
-
 @app.route("/niv")
 def nuv():
     return "<h1>.....<h1>"
 
+
+@app.route("/health")
+def is_alive():
+    resp = jsonify(success=True)
+    return resp
     
 if  __name__ == "__main__" :
     app.run(debug=True,host="0.0.0.0", port=8080)
